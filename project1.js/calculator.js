77b545886ab0f943067d9ae7d7573bbc93e671cd
kartikay650 <89460825+kartikay650@users.noreply.github.com>
2021-08-24 17:57:10 +0530

    Add files via upload

---
+++ project1.js/calculator.js
@@ -0,0 +1,10 @@
+A = 50 
+B = 15 
+sum = A + B
+diff = A - B
+mul = A*B
+div = A/B
+console. log("the sum of 10 and 20 is",sum)
+console. log("the difference of 10 and 20 is",diff)
+console. log("the multiplication of 10 and 20 is",mul)
+console. log("the division of 10 and 20 is",div)
